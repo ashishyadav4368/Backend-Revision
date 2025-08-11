e15163587d2c98bea8d1bee06c71a74c3598fdd6
Ashish <ay0393101@gmail.com>
2025-08-11 21:51:10 +0530

    database connection

---
+++ 6-Backend-Project-Setup/src/index.js
@@ -0,0 +1,38 @@
+// require('dotenv').config()
+import dotenv from "dotenv"
+
+import express from "express";
+import connectDB from "./db/index.js";
+const app = express();
+dotenv.config({
+    path: './.env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ; (async () => {
+//     try {
+//         await mongoose.connect(`{process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("Error:", error)
+//             throw error
+//         })
+//         app.listen(process.env.PORT, () => {
+//             console.log("server is running")
+//         })
+//     } catch (error) {
+//         console.error("Error: ", error)
+//     }
+// })();
